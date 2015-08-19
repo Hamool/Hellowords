a6e3e843acf58542577d060f61d29bcef1181ffe
Hamool <hamool111@gmail.com>
2015-08-19 18:14:20 +0600

    Added stuff

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("What's the fox say? ");
+            Console.WriteLine("the fox says: {0} {1} {2}", Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            Console.ReadKey();
+            Console.WriteLine("Thy Key Has Been Pressed.");
+            for (short i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Variable i={0}", i);
+            }
+        }
+    }
+}
